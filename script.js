624d69172eae0005f273b94e262547d65347e413
anupambaburaj <anupambaburaj@gmail.com>
2025-12-17 19:11:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ const headerContent = `
 const footerContent = `
     <footer>
         <div class="footer-info">
-            <p>Contact us: <a href="tel:+19803692983,1">+1 (980) 369-2983 ext:1</a></p>
-            <p>Follow us: <a href="https://www.facebook.com/cltomkar/" target="_blank">Facebook</a></p>
-            <p>&copy; 2025 Omkar Shakha All rights reserved.</p>
+            <p>Contact us: <a href="tel:+19803692983,1">+1 (980) 369-2983 ext:1</a>
+            Follow us: <a href="https://www.facebook.com/cltomkar/" target="_blank">Facebook</a>
+            &copy; 2025 Omkar Shakha All rights reserved.</p>
         </div>
     </footer>
 `;
